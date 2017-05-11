e6657d1cd385fa1cf79ffd6b352cc4a987bd204c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-11 11:50:22 -0400

    migrate python selftest

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+req = ['nose','numpy','matplotlib','seaborn']
+pipreq=[]
+# %%
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception:
+    pip.main(['install'] + req)
+#pip.main(['install'] + pipreq)
+# %%
+import setuptools # enables develop
+from numpy.distutils.core import Extension, setup
+
+src = 'cglow.f90 glow.f90 bands.f90 conduct.f90 egrid.f90 ephoto.f90 etrans.f90 exsect.f fieldm.f gchem.f90 geomag.f90 maxt.f90 mzgrid.f90 qback.f90 rcolum.f90 rout.f90 snoem.f90 snoemint.f90 solzen.f90 ssflux.f90 iri90.f nrlmsise00.f'.split(' ')
+
+ext = Extension(name='glow', sources=src, 
+                 f2py_options=['--quiet'])
+
+
+setup(name='glowiono',
+      packages=['glowiono'],
+      version='1.0',
+      author=['Stan Solomon','Liam Kilcommons','Michael Hirsch, Ph.D.'],
+      url = 'https://github.com/NCAR/GLOW',
+      description='GLobal airglOW model',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'Topic :: Scientific/Engineering :: Atmospheric Science',
+      'Programming Language :: Python :: 3',
+      ],
+      ext_modules=[ ext ],
+      data_files=[],
+    )
---
+++ tests/test.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+"""
+Registration testing of GLOW
+Michael Hirsch
+
+"""
+import logging
+from datetime import datetime
+from itertools import chain
+from numpy import array,zeros,float32,log,isclose,nan
+from numpy.testing import assert_allclose,run_module_suite
+#
+#from glowiono import makeeigen
+from sciencedates import datetime2yd,datetime2gtd
+#
+import glow
+msise00=None
+#%% test inputs
+z = list(range(30,110+1,1))
+z += (
+         [111.5,113.,114.5,116.] +
+         list(chain(range(118,150+2,2),range(153,168+3,3),range(172,180+4,4),
+                    range(185,205+5,5),range(211,223+6,6),range(230,244+7,7),
+                    range(252,300+8,8),range(309,345+9,9),range(355,395+10,10),
+                    range(406,428+11,11))) +
+         [440,453,467,482,498,515,533,551] +
+         list(range(570,950+20,20))
+         )
+z = array(z)
+
+nbins = 190; jmax=170 #glow.h
+
+eflux = 1.
+e0 = 1e3
+maxind = 112
+glat = 70; glon=0 #like aurora.in
+ap=4; f107=100; f107a=100
+nmaj=3; nst=6
+dtime = datetime(1999,12,21)
+#
+yd,utsec = datetime2yd(dtime)[:2]
+
+def test_egrid_maxt():
+    ener,dE = glow.egrid(199)
+    assert_allclose(ener[[maxind,maxind+10,-1]],[1017.7124,1677.9241,75005.171875],rtol=1e-5)
+#%% test of maxt
+    phi = glow.maxt(eflux,e0,ener, dE, itail=0, fmono=nan, emono=nan)
+    assert phi.argmax() == maxind
+    assert_allclose(phi[[maxind,maxind+10]],[ 114810.6,97814.438])
+
+#%% test vquart (quartic root) KNOWN DEFECTIVE FORTRAN ALGORITHM
+#Aquart = tile([-1,0,0,0,1],(jmax,1))
+#qroot = glow.vquart(Aquart,1)
+#assert_allclose(qroot[0],roots(Aquart[0,-1]))
+#Aquart = array([[-1,0,0,0,1],
+#                [-1,0,0,1,1]])
+#nq = Aquart.shape[0]
+#Aquart = tile(Aquart,(jmax//nq,1))
+#qroot = glow.vquartmod.vquart(Aquart, nq)
+#try:
+#    assert_allclose(qroot[:nq],
+#                    [1,0.8191725133961643])
+#except AssertionError as e:
+#    print('this mismatch is in discussion with S. Solomon.   {}'.format(e))
+
+def test_solzen():
+    sza = glow.solzen(yd,utsec,glat,glon)
+    assert isclose(sza, 133.43113708496094)
+    return sza
+
+#def test_snoem():
+#    doy = datetime2gtd(dtime)[0]
+#    zno,maglat,nozm = glow.snoem(doy,1.75*log(0.4*ap),f107)
+ #   assert_allclose((nozm[12,15],nozm[-2,-1]),(35077728.0, 1.118755e+08))
+#    return nozm
+
+def test_snoemint():
+    if msise00 is not None:
+        densd,tempd = rungtd1d(dtime,z,glat,glon,f107a,f107,[ap]*7)
+    # (nighttime background ionization)
+        print(tempd)
+        znoint = glow.snoemint(dtime.strftime('%Y%j'),glat,glon,f107,ap,z,
+                                   tempd.loc[:,'Tn'])
+        assert_allclose(znoint[[28,143]], (1.262170e+08,  3.029169e+01),rtol=1e-5) #arbitrary
+        return znoint
+
+#def test_fieldm():
+#    xdip,ydip,zdip,totfield,dipang,decl,smodip = glow.fieldm(glat,glon%360,z[50])
+#    assert isclose(xdip,0.1049523800611496)
+ #   assert isclose(totfield,0.5043528079986572)
+#    assert isclose(dipang,77.72911071777344)
+
+#def test_ssflux():
+#    iscale=1; hlybr=0.; hlya=0.; fexvir=0.; heiew=0.; xuvfac=3.
+#    wave1,wave2,sflux = glow.ssflux(iscale,f107,f107a,hlybr,fexvir,hlya,heiew,xuvfac)
+#    assert_allclose(sflux[[11,23]],(4.27225743e+11,   5.54400400e+07))
+
+def test_rcolum_qback():
+    if msise00 is not None:
+        densd,tempd = rungtd1d(dtime,z,glat,glon,f107a,f107,[ap]*7)
+
+        """ VCD: Vertical Column Density """
+        sza = test_solzen()
+        zcol,zvcd = glow.rcolum(sza,z*1e5,
+                                    densd.loc[:,['O','O2','N2']].values.T,
+                                    tempd.loc[:,'Tn'])
+    # FIXME these tests were numerically unstable (near infinity values)
+        assert isclose(zcol[0,0], 1e30) #see rcolum comments for sun below horizon 1e30
+        assert isclose(zvcd[2,5],5.97157e+28,rtol=1e-2) #TODO changes a bit between python 2 / 3
+    #%% skipping EPHOTO since we care about night time more for now
+        znoint = test_snoemint()
+        # zeros because nighttime
+        photoi = zeros((nst,nmaj,jmax),dtype=float32,order='F')
+        phono = zeros((nst,jmax),dtype=float32,order='F')
+        glow.qback(zmaj=densd.loc[:,['O','O2','N2']].values.T,
+                                    zno=znoint,
+                                    zvcd=zvcd,
+                                    photoi=photoi,phono=phono)
+        #arbitrary point check
+        assert isclose(photoi[0,0,77],1.38091e-18,rtol=1e-5)
+        assert isclose(phono[0,73],0.0,rtol=1e-5)
+    else:
+        logging.warning('skipped rcolum qback due to missing external msise00')
+
+#def test_glow():
+    # electron precipitation
+    #First enact "glow" subroutine, which calls QBACK, ETRANS and GCHEM among others
+
+#    glow.glow() #no args
+
+    #%% ver and constituants
+#    """
+#    currently using common block CGLOW, in future use module
+#    """
+ #   zceta = glow.cglow.zceta.T
+ #   zeta = glow.cglow.zeta.T[:,:11]
+#    zcsum = zceta.sum(axis=-1)[:,:11]
+#    assert_allclose(zcsum,zeta,rtol=1e-6)
+
+#def test_eigen():
+#    ener,dE = glow.egrid()
+#    ver,photIon,isr,phitop,zceta,sza,prates,lrates,tezs,sion=makeeigen(ener,ones_like(ener),dtime,(glat,glon))
+
+if __name__ == '__main__':
+    #test_snoemint()
+    run_module_suite()
